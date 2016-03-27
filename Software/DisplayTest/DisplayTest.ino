20da5900178b07265756de89f9351f20df78c267
TravisKraemer <travis.kraemer@gmail.com>
2016-03-26 22:37:50 -0500

    Fixed Some Bugs
    The LEDs now light up, seven segment display not working correctly
    though. Appears to be off by an output or two.

--- Software/DisplayTest/DisplayTest.ino
+++ Software/DisplayTest/DisplayTest.ino
@@ -7,56 +7,66 @@ const int NUM_DISPLAY_SEGMENTS = 8; //7 segment with dot
 const int NUM_SEVEN_SEGS = 5; //5 7 segment displays
 const int NUM_RGB_LINES = 3; //1 for each color
 const int NUM_LEDS_PER_ROW = 32; //max 32 LEDs per row (some have 30)
-//Seven Segment Characters          abcdefgh
-const unsigned char SEVEN_SEG_1 = 0b01100000;
+//Seven Segment Characters           abcdefgh
+const unsigned char SEVEN_SEG_1 = ~0b01100000;
 //---------------------------------------------------
 
 //---------------------------------------------------
 //PINS LIST
 //---------------------------------------------------
-const int SHIFT_CLOCK_PIN = 1; // Pin 1_0
-const int SERIAL_DATA_PIN = 0; // Pin 1_1
-const int STORAGE_CLOCK_PIN = 2; // Pin 1_2
+const int SHIFT_CLOCK_PIN = 2; // Pin 1_2
+const int SERIAL_DATA_PIN = 0; // Pin 1_0
+const int STORAGE_CLOCK_PIN = 1; // Pin 1_1
 const int BUTTON_IN_PIN = 3; // Pin 1_3
 
-const int SERIAL_HIGH = 1 << SHIFT_CLOCK_PIN;
-const int SERIAL_LOW = 0;
-const int SERIAL_LOW_CLOCK = 1 << SHIFT_CLOCK_PIN;
-const int SERIAL_HIGH_CLOCK = SERIAL_HIGH | SERIAL_LOW_CLOCK;
+const unsigned char SERIAL_HIGH = 0x01;
+const unsigned char SERIAL_LOW = 0x00;
+const unsigned char SERIAL_LOW_CLOCK = 0x04;
+const unsigned char SERIAL_HIGH_CLOCK = 0x05;
 //---------------------------------------------------
 
 //---------------------------------------------------
 //Function Prototype Declarations
 //---------------------------------------------------
+void delay2(unsigned int ms);
 int calcLED(int peg);
 int calcRow(bool player2, int peg);
 inline void shiftOutLow();
 inline void shiftOutHigh();
 inline void pulseStorageClock();
-void updateDisplay(int button, unsigned char sevenChar, 
-                    unsigned char sevenDispSelect, unsigned char rgb, bool player2, int peg);
+void updateDisplay(int button, unsigned char sevenChar, unsigned char sevenDispSelect, unsigned char rgb, bool player2, int peg);
 //---------------------------------------------------
-void setup()
+int main()
 {
   // put your setup code here, to run once:
   WDTCTL = WDTPW | WDTHOLD;		// Stop watchdog timer
-  P1DIR |= 1 << SHIFT_CLOCK_PIN | 1 << SERIAL_DATA_PIN | 1 << STORAGE_CLOCK_PIN; // Set pins to output direction
-}
-
-void loop()
-{
-  // put your main code here, to run repeatedly:
+  P1DIR = (1 << SHIFT_CLOCK_PIN) | (1 << SERIAL_DATA_PIN) | (1 << STORAGE_CLOCK_PIN); // Set pins to output direction
+  DCOCTL &= ~0xE0;
+  DCOCTL |= CALDCO_16MHZ;
+  BCSCTL1 &= ~0x0F;
+  BCSCTL1 |= CALBC1_16MHZ;
   int peg = -1;
+  bool player = false;
   for(;;)
   {
-    updateDisplay(0,SEVEN_SEG_1,0,255,false,peg);
+    updateDisplay(0,SEVEN_SEG_1,0,0,player,peg);
     peg++;
     if(peg > 120)
     {
       peg = -1;
+      player = !player;
     }
-    delay(100);
+    delay2(10);
   }
+  return 0;
+}
+
+void delay2(unsigned int ms)
+{
+ while (ms--)
+    {
+        __delay_cycles(16000); // 16000 set for 16Mhz change it to 1000 for 1 Mhz
+    }
 }
 
 //Updates the displays/LEDs with the data entered
@@ -69,7 +79,7 @@ void loop()
 //int peg - position of peg (front starting position is 0, back is -1)
 void updateDisplay(int button, unsigned char sevenChar, unsigned char sevenDispSelect, unsigned char rgb, bool player2, int peg)
 {
-  for(int i = 0; i < NUM_BUTTONS; i++)
+  for(int i = 1; i <= NUM_BUTTONS; i++)
   {
     if(i == button)
     {
@@ -81,7 +91,7 @@ void updateDisplay(int button, unsigned char sevenChar, unsigned char sevenDispS
     }
   }
   int rowOn = calcRow(player2, peg);
-  for(int i = 0; i < NUM_ROWS; i++)
+  for(int i = 1; i <= NUM_ROWS; i++)
   {
     
     if(i == rowOn)
@@ -93,9 +103,9 @@ void updateDisplay(int button, unsigned char sevenChar, unsigned char sevenDispS
       shiftOutHigh();//Set Serial out high and cycle clock
     }
   }
-  for(int i = 0; i < (1 << NUM_DISPLAY_SEGMENTS) ; i << 1)
+  for(int i = 0; i < NUM_DISPLAY_SEGMENTS; i++)
   {
-    if(sevenChar & i)
+    if(sevenChar & (1 << i))
     {
       shiftOutLow();//Set Serial out low and cycle clock
     }
@@ -115,9 +125,9 @@ void updateDisplay(int button, unsigned char sevenChar, unsigned char sevenDispS
       shiftOutHigh();//Set Serial out high and cycle clock
     }
   }
-  for(int i = 0; i < (1 << NUM_RGB_LINES); i << 1)
+  for(int i = 0; i < NUM_RGB_LINES; i++)
   {
-    if(i & rgb)
+    if((1 << i) & rgb)
     {
       shiftOutLow();//Set Serial out low and cycle clock
     }
@@ -141,22 +151,25 @@ void updateDisplay(int button, unsigned char sevenChar, unsigned char sevenDispS
   pulseStorageClock();
 }
 
-inline void shiftOutHigh()
+void shiftOutHigh()
 {
   P1OUT = SERIAL_HIGH; //Serial clock low and serial data high
   P1OUT = SERIAL_HIGH_CLOCK; //Clock goes high
+  P1OUT = SERIAL_HIGH; //Serial clock low and serial data high
 }
 
-inline void pulseStorageClock()
+void pulseStorageClock()
 {
-  P1OUT |= 1 << STORAGE_CLOCK_PIN; //Set Storage clock to high
+  P1OUT = 0;
+  P1OUT = 1 << STORAGE_CLOCK_PIN; //Set Storage clock to high
   P1OUT = 0; // set P1out to low
 }
 
-inline void shiftOutLow()
+void shiftOutLow()
 {
   P1OUT = SERIAL_LOW; //Serial clock and serial data low
   P1OUT = SERIAL_LOW_CLOCK; //Clock goes high
+  P1OUT = SERIAL_LOW; //Serial clock and serial data low
 }
 
 //Returns the row that is used to display the peg
